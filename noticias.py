8c7922e60e96499e71571a0d8afd70e55ca89b3a
edso2103 <65740725+edso2103@users.noreply.github.com>
2023-04-20 18:34:07 -0500

    Add files via upload

---
+++ noticias.py
@@ -0,0 +1,13 @@
+import boto3
+import urllib.request
+import datetime
+
+def download_page(url, bucket,nombre):
+    response = urllib.request.urlopen(url)
+    content = response.read().decode('utf-8')
+    today = datetime.date.today().strftime('%Y-%m-%d')
+    s3 = boto3.resource('s3')
+    s3.Object(bucket, 'headlines/raw/{}{}.html'.format(nombre,today)).put(Body=content)
+
+download_page('https://www.eltiempo.com', 'bucket-news21','elTiempo-')
+download_page('https://www.elespectador.com', 'bucket-news21','elEspectador-')
