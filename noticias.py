8e488960443edb5487cabeb6c7ce5a9d896d5f85
Ubuntu <ubuntu@ip-172-31-36-254.ec2.internal>
2023-04-23 04:12:26 +0000

    Agregando pruebas unitarias

--- noticias.py
+++ noticias.py
@@ -2,12 +2,17 @@ import boto3
 import urllib.request
 import datetime
 
-def download_page(url, bucket,nombre):
+
+def download_page(url, bucket, nombre):
+
     response = urllib.request.urlopen(url)
     content = response.read().decode('utf-8')
     today = datetime.date.today().strftime('%Y-%m-%d')
     s3 = boto3.resource('s3')
-    s3.Object(bucket, 'headlines/raw/{}{}.html'.format(nombre,today)).put(Body=content)
+    s3.Object(bucket, 'headlines/raw/{}{}.html'.format(
+        nombre, today)).put(Body=content)
+    return content
 
-download_page('https://www.eltiempo.com', 'bucket-news21','elTiempo-')
-download_page('https://www.elespectador.com', 'bucket-news21','elEspectador-')
+
+download_page('https://www.eltiempo.com', 'bucket-news21', 'elTiempo-')
+download_page('https://www.elespectador.com', 'bucket-news21', 'elEspectador-')
--- test_noticias.py
+++ test_noticias.py
@@ -1,4 +1,13 @@
 from noticias import download_page
+from unittest import mock
 
-def test_obtenerNoticias():
-    assert 1==1
+
+def test_download_page(mocker):
+
+    content = "<html><head><title>Example</title></head> \
+    <body><h1>Example</h1></body></html>"
+    mocker.patch('urllib.request.urlopen', return_value=mock.Mock(
+        read=lambda: content.encode()))
+    respuesta = download_page(
+        'https://www.eltiempo.com', 'bucket-news21', 'elTiempo-')
+    assert respuesta == content
